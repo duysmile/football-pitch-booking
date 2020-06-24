8ba696255162cdecc84bb4e71a30b29a40f20f80
ba-p <phuongtr4916@gmail.com>
2020-06-24 10:47:04 +0700

    Fix scss

--- client/src/components/JS/Home.js
+++ client/src/components/JS/Home.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import { Carousel, Button } from 'antd';
 import { Link } from "react-router-dom";
-import '../CSS/Home.scss';
+import cls from '../SCSS/Home.module.scss';
 
 export default function Home(){
     return(
     <div>
         <Carousel autoplay>
-            <div className="car-1">
+            <div className={cls.car1}>
                 <h4>.</h4>
             </div>
             
-            <div className="car-2">
+            <div className={cls.car2}>
                 <h4>.</h4>
             </div>
-            <div className="car-3">
+            <div className={cls.car3}>
                 <h4>.</h4>
             </div>
-            <div className="car-4">
+            <div className={cls.car4}>
                 <h4>.</h4>
             </div>
         </Carousel>
-        <div className="last-car">
+        <div className={cls.lastCar}>
             <Button type="primary" danger>
                 <Link to='/login' >For Rent</Link>
             </Button>
