d28cf8bd397c8d4cfa04b761cbf5044559e37681
ba-p <phuongtr4916@gmail.com>
2020-06-25 22:29:13 +0700

    Design Home page

--- client/src/components/JS/Home.js
+++ client/src/components/JS/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import cls from '../SCSS/Home.module.scss';
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import NavbarCollapse from 'react-bootstrap/NavbarCollapse';
 
 export default function Home(){
@@ -14,7 +14,7 @@ export default function Home(){
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <div style={{marginLeft:100}}>
+                <div className={cls.navBar}>
                     <Nav className="mr-auto">
                         <Nav.Item className={cls.navItem}>
                             <Link className={cls.link} to='/'>Home</Link>
@@ -26,7 +26,7 @@ export default function Home(){
                         <Nav.Item className={cls.navItem}>Tournaments</Nav.Item>
                         <Nav.Item className={cls.navItem}>Hired</Nav.Item>
                         <Nav.Item className={cls.navButton}>
-                            <Link className={cls.link} to='/login'>
+                            <Link className={cls.link} to='/login' >
                                 <span className={cls.button}>Login</span>
                             </Link>
                         </Nav.Item>
@@ -35,6 +35,17 @@ export default function Home(){
                     </div>
                 </Navbar.Collapse>
             </Navbar>
+        </div>
+        <div className={cls.body1}>
+            <div className="col-lg-6">
+                <img className={cls.img1} src={process.env.PUBLIC_URL + '/Calendar.svg'}></img>
+            </div>
+            <div className="col-lg-5 header-title-section text-center text-lg-left mx-lg-auto align-items-lg-start">
+                <p className={cls.headerSubtitle}>Pitching calendar</p>
+                <h1 className={cls.headerTitle}>Quickly activities, convinent</h1>
+                <p className={cls.headerTitleTest}>Description</p>
+            </div>
+            
         </div>
     </div>
     )
