1a92055437f0b4836052f3af399a1424847355f4
ba-p <phuongtr4916@gmail.com>
2020-06-25 15:39:16 +0700

    nav-bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Routes from './routes/index';
 import 'antd/dist/antd.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
 function App() {
--- client/src/components/JS/Home.js
+++ client/src/components/JS/Home.js
@@ -1,34 +1,41 @@
 import React from 'react';
-import { Carousel, Button } from 'antd';
 import { Link } from "react-router-dom";
 import cls from '../SCSS/Home.module.scss';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import NavbarCollapse from 'react-bootstrap/NavbarCollapse';
 
 export default function Home(){
     return(
     <div>
-        <Carousel autoplay>
-            <div className={cls.car1}>
-                <h4>.</h4>
-            </div>
-            
-            <div className={cls.car2}>
-                <h4>.</h4>
-            </div>
-            <div className={cls.car3}>
-                <h4>.</h4>
-            </div>
-            <div className={cls.car4}>
-                <h4>.</h4>
-            </div>
-        </Carousel>
-        <div className={cls.lastCar}>
-            <Button type="primary" danger>
-                <Link to='/login' >For Rent</Link>
-            </Button>
-            <Button type="primary" style={{marginLeft: 50}}>
-                <Link to='/rent'>Rent</Link>
-                </Button>
+        <div className={cls.header}>
+        <Navbar bg="#fff" expand="lg">
+                <Navbar.Brand>
+                    <img src={process.env.PUBLIC_URL + '/logo.jpg'} className={cls.logo} style={{marginLeft: 30}}></img>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <div style={{marginLeft:100}}>
+                    <Nav className="mr-auto">
+                        <Nav.Item className={cls.navItem}>
+                            <Link className={cls.link} to='/'>Home</Link>
+                        </Nav.Item>
+                        <Nav.Item className={cls.navItem}>
+                            <Link className={cls.link} to='/rent'>Rent</Link>
+                            </Nav.Item>
+                        <Nav.Item className={cls.navItem}>Around</Nav.Item>
+                        <Nav.Item className={cls.navItem}>Tournaments</Nav.Item>
+                        <Nav.Item className={cls.navItem}>Hired</Nav.Item>
+                        <Nav.Item className={cls.navButton}>
+                            <Link className={cls.link} to='/login'>
+                                <span className={cls.button}>Login</span>
+                            </Link>
+                        </Nav.Item>
+                       
+                    </Nav>
+                    </div>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
-  </div>
+    </div>
     )
 }
