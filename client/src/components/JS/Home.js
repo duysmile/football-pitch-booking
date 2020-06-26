6c94bc0f4bf3d33f0aa4424685bf7a224a2ce640
ba-p <phuongtr4916@gmail.com>
2020-06-26 14:33:15 +0700

    Done

--- client/src/components/JS/Home.js
+++ client/src/components/JS/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import cls from '../SCSS/Home.module.scss';
+import cls from '../SCSS/Head.module.scss';
+import clsB from '../SCSS/Body.module.scss';
 import { Navbar, Nav } from 'react-bootstrap';
 import '../SCSS/Home.scss'
 
@@ -42,10 +43,65 @@ export default function Home(){
             </div>
             <div className="col-lg-5 header-title-section text-center text-lg-left mx-lg-auto align-items-lg-start">
                 <p className={cls.headerSubtitle}>Booking football pitch</p>
-                <h1 className={cls.headerTitle}>Simple operation, quickly</h1>
-                <p className={cls.headerTitleTest}>Booking in the easiest way</p>
+                <h1 className={cls.headerTitle}>Simple Operation, Quickly.</h1>
+                <p className={cls.headerTitleTest}>Booking in the easiest way, it is very convenient and accordant for everyone with a few simple steps</p>
             </div>
-            
+        </div>
+        <div className='container-fluid strategy-section-container'>
+            <div className='container strategy-section d-md-flex flex-wrap'>
+                <div className='stragegy-card-section my-5 col-md-6 col-lg-3'>
+                    <div className={clsB.strategyCard}>
+                        <div className='strategy-card-icon'>
+                            <img src={process.env.PUBLIC_URL + '/calendar.png'} className={clsB.imgBody} width={95}></img>
+                        </div>
+                        <h2 className={clsB.titleStrategy + ' my-3'}>
+                            Booking
+                        </h2>
+                        <p className={clsB.subTitleStrategy}>
+                            You only need find your field and book one time.
+                        </p>
+                    </div>
+                </div>
+                <div className='stragegy-card-section my-5 col-md-6 col-lg-3'>
+                    <div className={clsB.strategyCard}>
+                        <div className='strategy-card-icon'>
+                            <img src={process.env.PUBLIC_URL + '/correct.png'} className={clsB.imgBody} width={95}></img>
+                        </div>
+                        <h2 className={clsB.titleStrategy + ' my-3'}>
+                            Verified
+                        </h2>
+                        <p className={clsB.subTitleStrategy}>
+                            You have to verify your account by email you registed
+                        </p>
+                    </div>
+                </div>
+                <div className='stragegy-card-section my-5 col-md-6 col-lg-3'>
+                    <div className={clsB.strategyCard}>
+                        <div className='strategy-card-icon'>
+                            <img src={process.env.PUBLIC_URL + '/money.png'} className={clsB.imgBody} width={95}></img>
+                        </div>
+                        <h2 className={clsB.titleStrategy + ' my-3'}>
+                            Deposit
+                        </h2>
+                        <p className={clsB.subTitleStrategy}>
+                            You must deposit 50% of your price and this is the last step
+                        </p>
+                    </div>
+                </div>
+                <div className='stragegy-card-section my-5 col-md-6 col-lg-3'>
+                    <div className={clsB.strategyCard}>
+                        <div className='strategy-card-icon'>
+                            <img src={process.env.PUBLIC_URL + '/reminder.png'} className={clsB.imgBody} width={95}></img>
+                        </div>
+                        <h2 className={clsB.titleStrategy + ' my-3'}>
+                            Remind
+                        </h2>
+                        <p className={clsB.subTitleStrategy}>
+                            We will remind two hours before taking your fields
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     )
