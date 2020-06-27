338a6d42561c0dabd888ac5fa6145b42ef5de781
ba-p <phuongtr4916@gmail.com>
2020-06-27 10:09:32 +0700

    Design

--- client/src/components/JS/Rent.js
+++ client/src/components/JS/Rent.js
@@ -1,11 +1,68 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import home from '../SCSS/Head.module.scss';
 import Navbar from '../layout/Navbar';
+import { List, Avatar, Space } from 'antd';
+
+const listData = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://ant.design',
+    title: `ant design part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 export default class Rent extends Component{
   render(){
     return(
-      <Navbar colorRent="#ff3e81"/>
+      <div>
+        <Navbar colorRent="#ff3e81"/>
+        <div className='container'>
+        <List
+    itemLayout="vertical"
+    size="large"
+    pagination={{
+      onChange: page => {
+        console.log(page);
+      },
+      pageSize: 3,
+    }}
+    dataSource={listData}
+    footer={
+      <div>
+        <b>ant design</b> footer part
+      </div>
+    }
+    renderItem={item => (
+      <List.Item
+        key={item.title}
+        extra={
+          <img
+            width={272}
+            alt="logo"
+            src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+          />
+        }
+      >
+        <List.Item.Meta
+          avatar={<Avatar src={item.avatar} />}
+          title={<a href={item.href}>{item.title}</a>}
+          description={item.description}
+        />
+        {item.content}
+      </List.Item>
+    )}
+  />
+
+
+
+
+
+        </div>
+      </div>
     )
   }
 }
