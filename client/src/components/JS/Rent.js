25c57a3cafd110c09348ad10a74261da0943f50f
duy21 <duy210697@gmail.com>
2020-07-07 20:10:02 +0700

    update ui for rent page

--- client/src/components/JS/Rent.js
+++ client/src/components/JS/Rent.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import Navbar from '../layout/Navbar';
+import { Card, Input, Space, Rate } from 'antd';
+
 import cls from '../SCSS/Rent.module.scss';
 import '../SCSS/Rent.scss';
 import ScrollToTop from 'react-scroll-up';
-import { List, Avatar, Input, Space, Select} from 'antd';
-import { CaretUpOutlined, StarOutlined } from '@ant-design/icons';
+import { Row, Col } from 'antd';
+import { CaretUpOutlined } from '@ant-design/icons';
 const { Search } = Input;
-const { Option } = Select;
 
 const IconText = ({ icon, text }) => (
   <Space>
@@ -29,74 +29,55 @@ for (let i = 0; i < 23; i++) {
       '',
   });
 }
-export default class Rent extends Component{
-  constructor(){
+export default class Rent extends Component {
+  constructor() {
     super();
     this.onSearch = this.onSearch.bind(this);
   }
-  onSearch(e){
+  onSearch(e) {
     let text = e.target.value;
     console.log(text);
   }
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <div className='container'>
           <div className={cls.filter}>
             <Search className={cls.searchField}
-                            placeholder="Field's name"
-                            onKeyUp={this.onSearch} 
-                            enterButton
-                    />
-            
+              placeholder="Field's name"
+              onKeyUp={this.onSearch}
+              enterButton
+            />
+
           </div>
-        <ScrollToTop showUnder={160}>
-            <CaretUpOutlined style={{ fontSize: '40px', color: '#ff3e81' }}/>
-        </ScrollToTop>
-        <List
-    className={cls.pagina}
-    itemLayout="vertical"
-    size="large"
-    pagination={{
-      onChange: page => {
-        console.log(page);
-      },
-      pageSize: 3,
-    }}
-    dataSource={listData}
-    renderItem={item => (
-      <List.Item
-        key={item.title}
-        actions={[
-          <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-        ]}
-        extra={
-          <img
-            width={272}
-            alt="logo"
-            src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-          />
-        }
-      >
-        <List.Item.Meta
-          // avatar={<Avatar src={item.avatar} />}
-          title={<a href={item.href}>{item.title}</a>}
-          description={item.description}
-        />
-        {
-          <Link className={cls.link} to= {`/rent/` + `${item.id}`}>
-            <span className={cls.button}>Hire</span>
-          </Link>
-        }
-        
-      </List.Item>
-    )}
-  />
+          <ScrollToTop showUnder={160}>
+            <CaretUpOutlined style={{ fontSize: '40px', color: '#ff3e81' }} />
+          </ScrollToTop>
 
-
-
-
-
+          <Row>
+            {listData.map((item, index) =>
+              <Col span={8} style={{ padding: '2rem' }}>
+                <Card
+                  key={index}
+                  style={{ width: '100%' }}
+                  cover={<img alt="example" src="https://static.vecteezy.com/system/resources/previews/000/182/533/non_2x/jumbotron-at-soccer-stadium-vector.jpg" />}
+                >
+                  <h5>
+                    {item.title}
+                  </h5>
+                  <Rate disabled defaultValue={2} style={{fontSize: '0.75rem'}} />
+                  <p>
+                    {item.description}
+                  </p>
+                  <div style={{ width: '100%' }}>
+                    <Link className={cls.button} to={`/rent/` + `${item.id}`}>
+                      Hire
+                    </Link>
+                  </div>
+                </Card>
+              </Col>
+            )}
+          </Row>
         </div>
       </div>
     )
