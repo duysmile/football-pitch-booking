584e82f18fea16a0bf67942d2d9a03c5f89d8b2f
Ba Phuong <phuongtr4916@gmail.com>
2020-07-09 19:36:12 +0700

    input

--- client/src/components/JS/Rent.js
+++ client/src/components/JS/Rent.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Card, Input, Space, Rate } from 'antd';
+import { Card, Input, Space, Rate, Pagination } from 'antd';
 
 import cls from '../SCSS/Rent.module.scss';
 import '../SCSS/Rent.scss';
 import ScrollToTop from 'react-scroll-up';
 import { Row, Col } from 'antd';
 import { CaretUpOutlined } from '@ant-design/icons';
-const { Search } = Input;
 
 const IconText = ({ icon, text }) => (
   <Space>
@@ -43,41 +42,43 @@ export default class Rent extends Component {
       <div>
         <div className='container'>
           <div className={cls.filter}>
-            <Search className={cls.searchField}
-              placeholder="Field's name"
-              onKeyUp={this.onSearch}
-              enterButton
-            />
-
+          <Input placeholder="Basic usage" className={cls.input}/>
+          <Select defaultValue="Zhejiang">
+        <Option value="Zhejiang">Zhejiang</Option>
+        <Option value="Jiangsu">Jiangsu</Option>
+      </Select>
           </div>
           <ScrollToTop showUnder={160}>
             <CaretUpOutlined style={{ fontSize: '40px', color: '#ff3e81' }} />
           </ScrollToTop>
-
-          <Row>
-            {listData.map((item, index) =>
-              <Col span={8} style={{ padding: '2rem' }}>
-                <Card
-                  key={index}
-                  style={{ width: '100%' }}
-                  cover={<img alt="example" src="https://static.vecteezy.com/system/resources/previews/000/182/533/non_2x/jumbotron-at-soccer-stadium-vector.jpg" />}
-                >
-                  <h5>
-                    {item.title}
-                  </h5>
-                  <Rate disabled defaultValue={2} style={{fontSize: '0.75rem'}} />
-                  <p>
-                    {item.description}
-                  </p>
-                  <div style={{ width: '100%' }}>
-                    <Link className={cls.button} to={`/rent/` + `${item.id}`}>
-                      Hire
-                    </Link>
-                  </div>
-                </Card>
-              </Col>
-            )}
-          </Row>
+          
+              <Row>
+                {listData.map((item, index) =>
+                
+                  <Col span={8} style={{ padding: '2rem' }}>
+                    <Card
+                      key={index}
+                      style={{ width: '100%' }}
+                      cover={<img alt="example" src="https://static.vecteezy.com/system/resources/previews/000/182/533/non_2x/jumbotron-at-soccer-stadium-vector.jpg" />}
+                    >
+                      <h5>
+                        {item.title}
+                      </h5>
+                      <Rate disabled defaultValue={2} style={{fontSize: '0.75rem'}} />
+                      <p>
+                        {item.description}
+                      </p>
+                      <div style={{ width: '100%' }}>
+                        <Link className={cls.button} to={`/rent/` + `${item.id}`}>
+                          Hire
+                        </Link>
+                      </div>
+                    </Card>
+                  </Col>
+           
+                )}
+              </Row>
+              
         </div>
       </div>
     )
