3f2beb1ad4a024eef5fb648c69aef48640e3f380
Ba Phuong <phuongtr4916@gmail.com>
2020-07-12 20:55:03 +0700

    Search

--- client/src/components/JS/Rent.js
+++ client/src/components/JS/Rent.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Card, Input, Space, Rate, Select, DatePicker, Button } from 'antd';
-import axios from 'axios';
+import { Card, Input, Space, Rate, Select, DatePicker } from 'antd';
 import cls from '../SCSS/Rent.module.scss';
 import '../SCSS/Rent.scss';
 import ScrollToTop from 'react-scroll-up';
 import { Row, Col } from 'antd';
 import { CaretUpOutlined } from '@ant-design/icons';
+import data from '../../city.json';
 
 const { Option } = Select; 
 const IconText = ({ icon, text }) => (
@@ -16,6 +16,8 @@ const IconText = ({ icon, text }) => (
   </Space>
 );
 
+let city = Object.keys(data);
+console.log(city)
 const listData = [];
 for (let i = 0; i < 23; i++) {
   listData.push({
@@ -29,40 +31,48 @@ for (let i = 0; i < 23; i++) {
       '',
   });
 }
-let city = [];
+
 export default class Rent extends Component {
   constructor() {
     super();
     this.onSearch = this.onSearch.bind(this);
+    this.state = {
+      listData: listData,
+      search: listData,
+      input: true
+    }
   }
-  onSearch(e) {
+   onSearch(e) {
     let text = e.target.value;
-    console.log(text);
-  }
-   componentDidMount(){
-
-    axios.get('https://iszbz.sse.codesandbox.io/city')
-    .then(function (res) {
-      // handle success
-      city = Object.keys(res.data);
-    console.log(city);
+    if(text === ''){
+      this.setState({search: this.state.listData});
+    }
+    let searched = listData.filter(function(e){
+      return e.title.toLowerCase().indexOf(text.toLowerCase()) !== -1;
     })
-    
+    this.setState({search: searched});
+
   }
+ 
+ 
   render() {
+    
     return (
+      
       <div>
         <div className='container'>
           <div className={cls.filter}>
-            <Input placeholder="Field's name" className={cls.input}/>
+            <Input placeholder="Field's name" className={cls.input} onKeyUp={this.onSearch}/>
             <DatePicker style={{marginRight: 20, marginBottom: 20}}/>
             <div className={cls.hometown}>
               <Select defaultValue="City" className={cls.city}>
                 {
-                  city.map((item, index) =>
-                  <Option value={item} className={cls.option}>{item}</Option>
-                 
-                )}
+                  city.map((item, index) => 
+                    
+                      <Option key={index} value={item} className={cls.option}>{item}</Option>
+ 
+                  )
+                }
               </Select>
               <Select defaultValue="District" className={cls.city}>
                 <Option value="Zhejiang" className={cls.option}>Binh Thanh</Option>
@@ -77,9 +87,8 @@ export default class Rent extends Component {
           <ScrollToTop showUnder={160}>
             <CaretUpOutlined style={{ fontSize: '40px', color: '#ff3e81' }} />
           </ScrollToTop>
-          
               <Row>
-                {listData.map((item, index) =>
+                {this.state.search.map((item, index) =>
                 
                   <Col style={{ padding: '2rem' }} className={cls.col}>
                     <Card
@@ -104,7 +113,7 @@ export default class Rent extends Component {
            
                 )}
               </Row>
-              
+           
         </div>
       </div>
     )
